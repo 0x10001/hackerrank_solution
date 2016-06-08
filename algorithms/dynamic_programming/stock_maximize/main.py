1a81a9b54675ca2199656b2fca784da60a0a9d96
Eric Wong <ericwong@zju.edu.cn>
2016-06-08 12:38:14 +0800

    DONE: stock maximize in dp

---
+++ algorithms/dynamic_programming/stock_maximize/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+   @author: Eric Wong
+  @license: MIT Licence 
+  @contact: ericwong@zju.edu.cn
+     @file: main.py
+     @time: 2016-06-07 23:12
+"""
+
+import sys
+
+DEBUG = 0
+
+if DEBUG:
+    handler = open("./input")
+else:
+    handler = sys.stdin
+
+cases = int(handler.readline())
+
+for _ in range(cases):
+    handler.readline()
+    prices = list(map(int, handler.readline().split()))[::-1]
+    peak = 0
+    profit = 0
+    for price in prices:
+        if peak < price:
+            peak = price
+        else:
+            profit += peak - price
+    print(profit)
+
+handler.close()
