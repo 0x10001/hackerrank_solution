2991ca3b7ce60836cd171d9f213c6dc6b1c4ed82
Eric Wong <ericwong@zju.edu.cn>
2016-06-09 15:51:15 +0800

    HALF DONE: grid walking in dp

--- algorithms/dynamic_programming/grid_walking/main.py
+++ algorithms/dynamic_programming/grid_walking/main.py
@@ -10,6 +10,7 @@
 """
 
 import sys
+from functools import reduce
 
 DEBUG = 1
 
@@ -23,7 +24,54 @@ MOD = 1000000007
 
 for _ in range(cases):
     dimensions, steps = map(int, handler.readline().split())
-    start = list(map(int, handler.readline().split()))
-    limits = list(map(int, handler.readline().split()))
+    start = tuple(map(int, handler.readline().split()))
+    limits = tuple(map(int, handler.readline().split()))
 
-    print(limits)
+    buf = {limits: 1}
+    '''
+    grid = []
+    num_grid = reduce(lambda x, y: x * y, limits, 1)
+    for grid_id in range(num_grid):
+        for limit in limits:
+            grid.append(grid_id % limit + 1)
+            grid_id //= limit
+        buf[tuple(grid)] = 0
+        grid.clear()
+    '''
+
+    grid = [1] * dimensions
+    while tuple(grid) < limits:
+        buf[tuple(grid)] = 1
+        grid[0] += 1
+        for d in range(dimensions):
+            if grid[d] > limits[d]:
+                grid[d] = 1
+                grid[d + 1] += 1
+            else:
+                break
+
+
+    def calc(g):
+        ret = 0
+        for d in range(dimensions):
+            front_slice, end_slice = g[:d], g[d + 1:]
+            upper = front_slice + (g[d] + 1,) + end_slice
+            lower = front_slice + (g[d] - 1,) + end_slice
+            if 1 < g[d]:
+                ret += buf[lower]
+            if g[d] < limits[d]:
+                ret += buf[upper]
+        return ret % MOD
+
+
+    for move in range(steps):
+        data = {}
+        if move == steps - 1:
+            print(calc(start))
+            break
+
+        for grid in buf:
+            data[grid] = calc(grid)
+        buf = data
+
+handler.close()
